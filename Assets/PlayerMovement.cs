4a815cf16106b1fc2fe592d465e1e01710174f38
SocateId <timothylim.10@gmail.com>
2020-11-26 23:56:51 +0800

    Added Animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f; // The Movement Speed (Left and Right) value
     float horizontalMove = 0f; // Keeps track of hozontal direction movement value (1, right or left, -1)
@@ -19,9 +20,16 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump")) {
             jump = true;
+            animator.SetBool("IsJumping", jump);
         }
+    }
+
+    public void OnLand() {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate() {
